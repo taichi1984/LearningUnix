632d3ad7d16c9958ee175ae6f106368b40c00b73
taichi84 <taichi84@gmail.com>
2026-03-03 23:57:33 +0900

    研究課題10.02完了

---
+++ chapter10/question/10.02/pipe.c
@@ -0,0 +1,63 @@
+/* pipe.c
+ * 目的：１つのプロセスから別のプロセスにデータを流す。
+ * パイプラインの作り方を示す。引数として２つのコマンドを取り、av[1]の出力をav[2]の出力に接続する。
+ *
+ *  使い方 pipe command1 command2
+ *  効果 : command 1 | command2
+ *  制限 : コマンドは引数を取ることができない。
+ *  引数の個数がわかっているのでexeclp()を使う。
+ *  注意：子と親を取り替えると面白いことになる。
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define oops(m, x)                                                             \
+  {                                                                            \
+    perror(m);                                                                 \
+    exit(x);                                                                   \
+  }
+
+int main(int ac, char **av) {
+  int thepipe[2], /*2つのファイルディスクリプタ*/
+      newfd, pid;
+
+  if (ac != 3) {
+    fprintf(stderr, "usage:pipe cmd1 cmd2\n");
+    exit(1);
+  }
+  if (pipe(thepipe) == -1)
+    oops("Cannot get a pipe", 1);
+
+  /* ----------------------------------------------------------
+   * パイプを作ったのでプロセスを２つにする */
+
+  if ((pid = fork()) == -1)
+    oops("Cannot fork", 2);
+
+  /* -----------------------------------------------------------
+   * この時点でプロセスは２個ある。親はパイプから読み出す*/
+
+  if (pid == 0) {      /*親はav[2]を実行する*/
+    close(thepipe[1]); /*親はパイプに書き込まない*/
+
+    if (dup2(thepipe[0], 0) == -1)
+      oops("could not redirect stdin", 3);
+
+    close(thepipe[0]); /*標準入力の複製を作り、パイプをクローズする。*/
+    execlp(av[2], av[2], NULL);
+    oops(av[2], 4);
+  }
+
+  /* 子はav[1]を実行し、パイプに書き込む*/
+  close(thepipe[0]); /*子はパイプから読み出さない*/
+
+  if (dup2(thepipe[1], 1) == -1)
+    oops("could not redirect stdout", 4);
+
+  close(thepipe[1]); /*標準出力の複製を作り、パイプをクローズする。*/
+
+  execlp(av[1], av[1], NULL);
+  oops(av[1], 5);
+}
